0965f518d96f61d373a63ace42475098820a8888
Jhonatan Noguera <jhonatan.noguera@correounivalle.edu.co>
2019-08-20 20:26:29 -0500

    adding collection item component, fixing classname spelling mistake on collection-preview

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import CollectionItem from '../collection-item/collection-item.component';
 
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
-    <div classname='collection-preview'>
-        <h1 classname='title'>{ title.toUpperCase() }</h1>
-        <div classname='preview'>
+    <div className='collection-preview'>
+        <h1 className='title'>{ title.toUpperCase() }</h1>
+        <div className='preview'>
             {
                 items
                 .filter((item, index) => index < 4)
-                .map(item => (
-                    <div key={item.id}>{item.name}</div>
+                .map(({id, ...itemProps}) => (
+                    <CollectionItem key={id} {...itemProps}/> 
                 ))
             }
         </div>
